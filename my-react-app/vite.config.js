1d81ec6a433ee7a752fb2a31412fc954c69cb614
Max4king <ryan3311@hotmail.com>
2024-05-06 11:47:59 +0200

    initial template for multi page

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import { mainModule } from 'process'
+
+const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname, 'dist')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root,
   plugins: [react()],
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+        login: resolve(root, 'login', 'login.html'),
+        register: resolve(root, 'register', 'register.html'),
+        dashboard: resolve(root, 'dashboard', 'dashboard.html'),
+      }
+    }
+  }
+  
 })
